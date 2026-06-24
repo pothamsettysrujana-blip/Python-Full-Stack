1efa62e2727af12a42cb5ed61e8a3ddbffe2e64d
pothamsettysrujana-blip <pothamsettysrujana@gmail.com>
2026-06-24 11:12:52 +0530

    Delete sample.py

--- sample.py
+++
@@ -1,8 +0,0 @@
-Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> print(4+6)
-10
->>> print(6*5)
-30
->>> print("Helllo World")
-Helllo World
