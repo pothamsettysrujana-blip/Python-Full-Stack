f1246df28c58c697decfcd17ac505f299396371f
pothamsettysrujana-blip <pothamsettysrujana@gmail.com>
2026-06-26 22:35:01 +0530

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,34 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> # Data Types
+>>> a=10
+>>> type(a)
+<class 'int'>
+>>> b=4.5
+>>> type(b)
+<class 'float'>
+>>> c='code'
+>>> type(c)
+<class 'str'>
+>>> d="codegnan"
+>>> type(d)
+<class 'str'>
+>>> e='''course'''
+>>> type(e)
+<class 'str'>
+>>> x=4+5j
+>>> type(x)
+<class 'complex'>
+>>> x4+5i
+SyntaxError: invalid decimal literal
+>>> a=True
+>>> type(a)
+<class 'bool'>
+>>> b=False
+>>> type(b)
+<class 'bool'>
+>>> a='True'
+>>> type(a)
+<class 'str'>
+>>> # DataType Conversions
+>>> #int
