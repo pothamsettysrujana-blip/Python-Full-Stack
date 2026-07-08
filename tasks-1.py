f4043e4bf7245687ce0dc12d542744d8eeca431e
pothamsettysrujana-blip <pothamsettysrujana@gmail.com>
2026-07-08 14:09:41 +0530

    Add files via upload

---
+++ tasks-1.py
@@ -0,0 +1,101 @@
+#integer
+#voting
+'''a=int(input("age:"))
+if a>=18:
+    print("eligible for vote")
+else:
+    print("not eligible for vote")'''
+#even or odd
+'''a=int(input("value:"))
+if a%2==0:
+    print("even")
+else:
+    print("odd")'''
+
+#leap year
+'''a=int(input("year:"))
+if a%4==0:
+    print("leap year")
+else:
+    print("notleap year")'''
+
+
+#string
+#guest code
+
+'''name=input("name:")
+if name=="srujana":
+    print("welcome srujana")
+else:
+    print("welcome guest")'''
+
+'''name=input("name:").lower()
+if name=="srujana":
+    print("welcome",name)
+else:
+    print("welcome guest")'''
+
+'''name=input("name:").title()
+if name=="Srujana":
+    print("welcome",name)
+else:
+    print("welcome guest")'''
+
+'''names=["srujana","viji","suni","yashu","aswini"]
+a=input("name:")
+if a in names:
+    print("welcome",a)
+else:
+    print("welcome guest")'''
+
+
+#vowels
+'''vowels=["a","e","i","o","u"]
+a=input("letter:")
+if a in vowels:
+    print("it is vowel")
+else:
+    print("it is consonant")'''
+
+#social media login    
+'''user_name=input("enter the user name:")
+password=int(input("enter the password:"))
+if user_name=="srujana":
+    if password==1234:
+        print("login successfull")
+    else:
+        print("incorect password")
+else:
+    print("Invalid credentials")'''
+
+'''a="srujana"
+b=1234
+user_name=input("enter the user name:")
+password=int(input("enter the password:"))
+if user_name==a:
+    if password==b:
+        print("login successfull")
+    else:
+        print("incorect password")
+else:
+    print("Invalid credentials")'''
+
+'''user_name=input("enter the user name:")
+password=input("enter the password:")
+if user_name=="srujana" and password==1234:
+    print("login successfull")
+else:
+    print("Invalid credentials")'''   
+
+#multiple if 
+'''age=int(input("age:"))
+marks=int(input("marks:"))
+attendence=int(input("attn:"))
+if age>=18:
+    print("eligible for vote")
+if marks>=80:
+    print("eligible for scholarship")
+if attendence>=70:
+    print("eligible for exam")
+else:
+    print("not eligible")'''
